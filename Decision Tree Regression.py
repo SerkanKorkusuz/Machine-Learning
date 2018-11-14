bae3fba6e4626bf3b7ae829ab133ba632c5f1639
Serkan KORKUSUZ <42834460+SerkanKorkusuz@users.noreply.github.com>
2018-11-14 23:28:52 +0300

    Create Decision Tree Regression.py

---
+++ Decision Tree Regression.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plot
+from sklearn.tree import DecisionTreeRegressor
+
+source_url = ("C:\\Users\\casper\\Desktop\\Machine Learning\\Machine Learning in Python\\Udemy-DATAI\\Decision Tree Regression\\decision-tree-regression-dataset.csv")
+myData = pd.read_csv(source_url, header = None, prefix = "V", sep = ";")
+print(myData)
+
+att = myData.V0.values.reshape(-1,1)
+label = myData.V1.values.reshape(-1,1)
+print(att)
+print(label)
+
+my_tree_model = DecisionTreeRegressor()
+my_tree_model.fit(att, label)
+
+print(my_tree_model.predict([[6]]))
+
+attX = np.arange(min(att), max(att), 0.01).reshape(-1,1)
+labelY = my_tree_model.predict(attX).reshape(-1,1)
+plot.scatter(att, label)
+plot.plot(attX, labelY, color = "orange")
+plot.xlabel("Tribune Sector")
+plot.ylabel("Ticket Price")
+plot.show()
