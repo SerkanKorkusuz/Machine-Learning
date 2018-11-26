4786feae87a8e3424c79615c649ee36599b36224
Serkan KORKUSUZ <42834460+SerkanKorkusuz@users.noreply.github.com>
2018-11-26 23:51:41 +0300

    Update Random Forest Regression.py

--- Random Forest Regression.py
+++ Random Forest Regression.py
@@ -9,3 +9,11 @@ from sklearn.metrics import r2_score
 source_url = ("C:\\Users\\casper\\Desktop\\Machine Learning\\Machine Learning in Python\\Udemy-DATAI\\Random forest Regression\\random-forest-regression-dataset.csv")
 myData = pd.read_csv(source_url, header = None, prefix = "V", sep = ";")
 print(myData)
+
+att = myData.iloc[:, 0].values.reshape(-1,1)
+label = myData.iloc[:, 1].values.reshape(-1, 1)
+
+my_rf_model = RandomForestRegressor(n_estimators = 100, random_state = 42)
+#n_estimator determines the numbers of trees
+my_rf_model.fit(att, label)
+print(my_rf_model.predict([[7.8]]))
