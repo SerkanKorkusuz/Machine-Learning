4be1c795eb74bd42193697fc3a1eba228a64c66e
Serkan KORKUSUZ <42834460+SerkanKorkusuz@users.noreply.github.com>
2018-11-27 00:30:13 +0300

    Update Random Forest Regression.py

--- Random Forest Regression.py
+++ Random Forest Regression.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plot
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import r2_score
 
-source_url = ("C:\\Users\\casper\\Desktop\\Machine Learning\\Machine Learning in Python\\Udemy-DATAI\\Random forest Regression\\random-forest-regression-dataset.csv")
+source_url = ("https://raw.githubusercontent.com/SerkanKorkusuz/Machine-Learning/master/random-forest-regression-dataset.csv")
 myData = pd.read_csv(source_url, header = None, prefix = "V", sep = ";")
 print(myData)
 
