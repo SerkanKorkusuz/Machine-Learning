20ca5f69f4060263187a614b1d331716496b7a68
Serkan KORKUSUZ <42834460+SerkanKorkusuz@users.noreply.github.com>
2018-11-28 22:54:52 +0300

    Create Naive Bayes.py

---
+++ Naive Bayes.py
@@ -0,0 +1,13 @@
+__machine_teacher__ = "serkan korkusuz"
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plot
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
+from pandas import DataFrame
+
+source_url = ("C:\\Users\\casper\\Desktop\\Machine Learning\\Machine Learning in Python\\Udemy-DATAI\\Naive Bayes\\data.csv")
+myData = pd.read_csv(source_url, header = 0)
+myData.drop(["id", "Unnamed: 32"], axis = 1, inplace = True)
+#print(myData.head())
