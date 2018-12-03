e42a0807ab57d2cd056cf574309db907aadbddb4
Serkan KORKUSUZ <42834460+SerkanKorkusuz@users.noreply.github.com>
2018-12-03 22:41:21 +0300

    Create K-Means Custering.py

---
+++ K-Means Custering.py
@@ -0,0 +1,15 @@
+__machine_teacher__ = "serkan korkusuz"
+
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plot
+from sklearn.cluster import KMeans
+
+att1 = np.random.normal(25, 5, 1000)
+label1 = np.random.normal(25, 5, 1000)
+
+att2 = np.random.normal(55, 5, 1000)
+label2 = np.random.normal(60, 5, 1000)
+
+att3 = np.random.normal(55, 5, 1000)
+label3 = np.random.normal(15, 5, 1000)
