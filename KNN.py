da47cad66460db04f45e596b7bd5edf317c490be
Serkan KORKUSUZ <42834460+SerkanKorkusuz@users.noreply.github.com>
2019-01-16 23:55:16 +0300

    Update KNN.py

--- KNN.py
+++ KNN.py
@@ -6,7 +6,9 @@ import matplotlib.pyplot as plot
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 
-source_url = ("C:\\Users\\casper\\Desktop\\Machine Learning\\Machine Learning in Python\\Udemy-DATAI\\KNN\\data.csv")
+source_url = ("https://raw.githubusercontent.com/SerkanKorkusuz/Machine-Learning/master/KNN-data.csv")
 myData = pd.read_csv(source_url, header = 0)
 myData.drop(["id", "Unnamed: 32"], axis = 1, inplace = True)
 #print(myData.head())
+
+#to be continued...
