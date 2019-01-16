1facec8cdcecdb8ff697c88c895cb71aa4cd84ec
Serkan KORKUSUZ <42834460+SerkanKorkusuz@users.noreply.github.com>
2019-01-16 23:53:37 +0300

    Create KNN.py

---
+++ KNN.py
@@ -0,0 +1,7 @@
+__machine_teacher__ = "serkan korkusuz"
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plot
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
